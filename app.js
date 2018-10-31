509c5e63c39ba2ebd667d0bb3c5df57fef335076
Jennifer Enck <jenniferenck@mac.com>
2018-10-30 17:36:46 -0700

    changes to js

--- app.js
+++ app.js
@@ -9,14 +9,33 @@ for (let j=0; j<values.length; j++){
     console.log(card[j].value);
 }
 
+let numFlips = 0;
+
+// turn over cards with click (only if there are less than 2 turned)
 for (let i=0; i<card.length; i++){ 
     card[i].addEventListener('click', ()=> {
         if (!card[i].classList.contains('in-play') && !card[i].classList.contains('matched') && document.getElementsByClassName('in-play').length < 2) {
             card[i].classList.add('in-play');
-        }        
+            let flippedCard = card[i]
+            console.log(flippedCard);
+            // call function to revert card back (removing 'in-play' class)
+            revertToOriginal(flippedCard);
+        }
+        numFlips ++; 
+        // console.log(numFlips);  
+        // if values are equal set class to 'matched'      
     })
 }
 
+// takes the card that is overturned and flips back with delay of 2s
+// setTimeout(flipCardBack, 3000); timeout was not working... come back
+
+function revertToOriginal (cardValue){
+    console.log('it worked');
+    console.log(cardValue);
+    cardValue.classList.remove('in-play');
+}
+ 
 if (document.getElementsByClassName('in-play').length === 2){
 
 }
