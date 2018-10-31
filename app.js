b545721c0fe84a4f066d00e1e0d52a864d8fb10c
Jennifer Enck <jenniferenck@mac.com>
2018-10-31 14:38:53 -0700

    modified styling, added start page, fixed timer requirements

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const card = document.getElementsByClassName('card');
-
+// card values: 
 const values = [1, 1, 2, 2, 3, 3];
 
+// scramble cards each page refresh
 
 // assign values --- will later need to scramble values
 for (let j=0; j<values.length; j++){
@@ -9,35 +10,49 @@ for (let j=0; j<values.length; j++){
     console.log(card[j].value);
 }
 
-let flippedValues = [];
-let scoreCount = 0;
-let turnCounter = 0;
+let flippedValues = []; // stores element values to compare for match
+let currentScore = 0; // player's score is increased by each valid click 
+let turnCounter = 0; //
 let pairsMatched = 0; // KEEP track of number to reach for game to finish
 
 // turn over cards with click (only if there are less than 2 turned)
-
     for (let i=0; i<card.length; i++){ 
         card[i].addEventListener('click', ()=> {
-            if (!card[i].classList.contains('in-play') && !card[i].classList.contains('matched') && turnCounter < 2) {
-                    
+            if (!card[i].classList.contains('in-play') && !card[i].classList.contains('matched') && turnCounter < 2) {   
+                
                 card[i].classList.add('in-play'); // change class to 'in-play'
+                removeClick(card[i], clickFunction) // removes clickability of same card...
                 flippedValues.push(card[i]);
                     
-                revertToOriginal(card[i]); // call function to revert card back (removing 'in-play' class)
+                turnCounter += 1; 
+                currentScore += 1; 
+                // call function to updateScoreBoard (currentScore)
+                updateScoreBoard(currentScore);
                      
                 if (turnCounter === 2){
                     console.log('you have 2 cards flipped'); // check for match
+                    // need to remove click for all other cards...
+                    removeAllClicks (card, clickFunction)
+                    setTimeout(flipBack, 1200, flippedValues[flippedValues.length-2], flippedValues[flippedValues.length-1]); // call function to revert card back (removing 'in-play' class)
                     if (flippedValues[flippedValues.length-2].value === flippedValues[flippedValues.length-1].value){
                         console.log("it's a match!");
                         newPair (flippedValues[flippedValues.length-2], flippedValues[flippedValues.length-1]);
+                        resetCounter();
+                    } else {
+                        setTimeout(resetCounter, 1100);
                     }
-                    turnCounter = 0; // resets turnCounter;
                 }    
             }  // END IF not 'in-play' or 'matched'
         
         }) // END callback for click
 
     } // END for loop
+
+// CALLBACK FUNCTIONS: 
+
+function resetCounter(){
+    turnCounter = 0;
+}
 
 function newPair (card1, card2){
     card1.classList.add('matched');
@@ -46,29 +61,34 @@ function newPair (card1, card2){
     card2.classList.remove('active');
 }
 
-
-// if there are 2 'in-play' cards ... STOPPED HERE - need to move to when you hit 2 cards...
 // takes the card that is overturned and flips back with delay of 2s
 // setTimeout(flipCardBack, 3000); timeout was not working... come back
 
-function revertToOriginal (cardEl){
+var flipBack = function revertToOriginal (card1, card2){
     console.log('card flipped back');
-    cardEl.classList.remove('in-play');
-    turnCounter += 1; 
-    scoreCount += 1;
-    console.log('You have flipped ' + scoreCount + ' cards')
+    card1.classList.remove('in-play');
+    card2.classList.remove('in-play');
+    console.log('You have flipped ' + currentScore + ' cards')
 }
  
-if (document.getElementsByClassName('in-play').length === 2){
-
+function updateScoreBoard (currentScore){
+    document.getElementById('score').innerText = currentScore;
 }
 
-function assignMatch (){
-    card[i]
+function removeClick (cardInplay, fn) {
+    cardInplay.removeEventListener('click', fn);
 }
-           
 
+function removeAllClicks (allCards, fn) {
+    for (let i=0; i<allCards.length; i++){
+        allCards[i].removeEventListener('click', fn)
+    }
+}
 
-// document.getElementsByClassName('in-play').length < 2
-
-//var displayCard = function ();
+function clickFunction () {
+    card[i].classList.add('in-play');
+    flippedValues.push(card[i]);               
+    turnCounter += 1; 
+    currentScore += 1; 
+    updateScoreBoard(currentScore);
+}
